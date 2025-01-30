0db048bd162346d9434e390d88cdaecd0283d77e
Pedro Deane <pedro.deane@gazeus.com>
2025-01-30 00:50:33 -0300

    header profile name with localization

--- Assets/Project/Scripts/Views/Lobby/HeaderView.cs
+++ Assets/Project/Scripts/Views/Lobby/HeaderView.cs
@@ -1,5 +1,13 @@
+#pragma warning disable UNT0006 // Incorrect message signature
+
 using System;
+using System.Diagnostics.CodeAnalysis;
+using Gazeus.Mobile.Domino.Managers;
+using Gazeus.Mobile.Domino.Services;
+using Gazeus.Mobile.Domino.Views.Prefabs;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Localization;
 using UnityEngine.UI;
 
 namespace Gazeus.Mobile.Domino.Views.Lobby
@@ -9,12 +17,30 @@ namespace Gazeus.Mobile.Domino.Views.Lobby
         [Header("UI")]
         [SerializeField] private Button _moreGamesButton;
         [SerializeField] private Button _settingsButton;
+        [SerializeField] private TextMeshProUGUI _profileNameText;
+
+        [Header("Localization")]
+        [SerializeField] private LocalizedString _guestProfileKey;
+
+        [Header("Views")]
+        [SerializeField] private AvatarView _avatarView;
 
         public event Action SettingsButtonClicked;
 
         private const string _moreGamesUrl = "https://play.google.com/store/apps/dev?id=6748044026530676626";
 
+        private ProfileService _profileService;
+        private VipService _vipService;
+
         #region Unity
+        private void Awake()
+        {
+            _profileService = GameManager.ServiceProviderManager.GetService<ProfileService>();
+            _vipService = GameManager.ServiceProviderManager.GetService<VipService>();
+
+            _guestProfileKey.StringChanged += GuestProfileKey_StringChanged;
+        }
+
         private void OnDisable()
         {
             _moreGamesButton.onClick.RemoveAllListeners();
@@ -25,6 +51,18 @@ namespace Gazeus.Mobile.Domino.Views.Lobby
         {
             _moreGamesButton.onClick.AddListener(OnMoreGamesButtonClick);
             _settingsButton.onClick.AddListener(OnSettingsButtonClick);
+        }
+
+        [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Unity Start method")]
+        private async Awaitable Start()
+        {
+            if (_profileService.IsLoggedIn())
+            {
+                Sprite avatarSprite = await _profileService.GetAvatarSpriteAsync();
+                _avatarView.SetAvatarSprite(avatarSprite);
+            }
+
+            _avatarView.SetAvatarVip(_vipService.IsVip);
         }
         #endregion
 
@@ -37,6 +75,11 @@ namespace Gazeus.Mobile.Domino.Views.Lobby
         private void OnSettingsButtonClick()
         {
             SettingsButtonClicked?.Invoke();
+        }
+
+        private void GuestProfileKey_StringChanged(string value)
+        {
+            _profileNameText.text = value;
         }
         #endregion
     }
